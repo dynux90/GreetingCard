7b6054b7818882ee48cbd3812658d80f0354a11f
davide.dagostino <davide.dagostino@siospa.it>
2025-05-07 14:56:18 +0200

    change sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.greetingcard"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.greetingcard"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
